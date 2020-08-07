84ae143e8f90bf8b21a47109ed0659b5825b5e01
alexqwerty2000 <ivanov.alex1@gmail.com>
2020-08-07 14:36:48 +0300

    Changed project

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from kivy.app import App 
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-import json
+from kivy.uix.image import Image
+from kivy.uix.behaviors import ButtonBehavior
+import json, glob
+import random
 from datetime import datetime
+from pathlib import Path
+from hoverable import HoverBehavior
 
 Builder.load_file("design.kv")
 
@@ -15,6 +20,9 @@ class LoginScreen(Screen):
             users = json.load(file)
         if uname in users and users[uname]['password'] == pword:
             self.manager.current = "login_screen_success"
+        else:
+            self.ids.login_wrong.text = "Wrong username or password" 
+
 
 class RootWidget(ScreenManager):
     pass
@@ -41,6 +49,21 @@ class LoginScreenSuccess(Screen):
     def log_out(self):
         self.manager.transition.direction = "right"
         self.manager.current = "login_screen"
+    
+    def get_quotes(self, feel):
+        feel = feel.lower()
+        available_quotes = glob.glob("quotes/*txt")
+        available_quotes = [Path(filename).stem for filename in available_quotes]
+
+        if feel in available_quotes:
+            with open(f"quotes/{feel}.txt") as file:
+                quotes = file.readlines()
+            self.ids.quote.text = random.choice(quotes)
+        else:
+            self.ids.quote.text = "Try another feeling"
+
+class ImageButton(HoverBehavior, ButtonBehavior, Image) :
+    pass
 
 class MainApp(App):
     def build(self):
